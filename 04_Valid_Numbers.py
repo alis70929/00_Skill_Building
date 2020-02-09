d9a81f11ca4cc23a041546d153653d48783aea25
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-02-10 09:53:07 +1300

    04 Valid numbers is working

--- 04_Valid_Numbers.py
+++ 04_Valid_Numbers.py
@@ -1,8 +1,11 @@
 #Ask user for a number
 # Loops question so it repeats until a valid number is entered
 # make code recyclable
-
+valid = False
 error = "Whoops, please enter an integer"
-
-try:    
-except TypeError:
+while not valid:
+    try:
+        response = int(input("Enter an integer between 1 - 10"))
+        valid = True
+    except ValueError:
+        print(error)
