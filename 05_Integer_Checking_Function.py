91fd573dd79650887ef9c168bbd3302631e1d11c
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-02-11 14:38:13 +1300

    Integer checker function working

--- 05_Integer_Checking_Function.py
+++ 05_Integer_Checking_Function.py
@@ -1,17 +1,23 @@
 #Ask user for a number
 # Loops question so it repeats until a valid number is entered
 # make code recyclable
-valid = False
-error = "Whoops, please enter an integer between 1 - 10"
-while not valid:
-    try:
-        response = int(input("Enter an integer between 1 - 10"))
-        if 1 <= response <= 10:
-            print(response)
-            valid = True
-        else:
+
+#Functions go here
+def intcheck(low,high):
+    valid = False
+    error = "Whoops, please enter an integer between {} - {}".format(low,high)
+    while not valid:
+        try:
+            response = int(input("Enter a integer between {} - {}".format(low,high)))
+            if low <= response <= high:
+                return response
+                valid = True
+            else:
+                print(error)
+                print()
+        except ValueError:
             print(error)
             print()
-    except ValueError:
-        print(error)
-        print()
+#Main routine goes here
+num_1 = intcheck(10,20)
+num_2 = intcheck(12,24)
